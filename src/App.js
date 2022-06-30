cb5ebba3f1a1823df104cc2b823bffe6f5fff733
Yakir <yakir@bmby.com>
2022-06-30 10:55:55 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import Tesseract from 'tesseract.js';
 
 function App() {
+  const [file,setFile] = useState();
+  const [progress,setProgress] = useState(0);
+  const [language,setlanguage] = useState("eng");
+  const [result,setResult] = useState("");
+  const onFileChange = (e) => {
+    // log the actual file that was uploaded by the user
+    //console.log(e.target.files[0]);
+    setFile(e.target.files[0]);
+  };
+   // after pressing submit, send the actual file to here
+  const processImage = () => {
+    // if user already uploaded an image beofore, reset the result text & progress bar from screen
+    if(result !== ""){
+      setResult("");
+
+      setProgress(0);
+    } 
+    console.log("selected file is:"+file);
+    console.log("selected language is:"+language);
+    //console.log(file);
+    Tesseract.recognize(
+        file,
+        language, 
+        { logger: result => {
+          if(result.status === 'recognizing text'){
+              setProgress(result.progress);
+            }
+        }
+      }).then(({ data: { text } }) => {
+         //console.log(text);
+         setResult(text);
+         });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" onChange={onFileChange}/>       
+              <select value={language} onChange= {(e) => setlanguage(e.target.value)}>
+                <option value="eng">English</option>
+                <option value="heb">Hebrew</option>
+                <option value="fra">French</option>
+                <option value="ara">Arabic</option>
+                <option value="rus">Russian</option>
+                <option value="spa">Spanish</option>
+              </select>  
+         <div className="submitButton">
+           <input type="button" value="Submit" onClick={processImage}/>
+         </div>    
+      <div className="progressBar">
+        <progress value={progress} max={1}/>
+      </div>
+      {result !== "" && (
+        <div className="textResult">
+          Result: {result}
+        </div>
+       )}
     </div>
   );
 }
-
 export default App;
